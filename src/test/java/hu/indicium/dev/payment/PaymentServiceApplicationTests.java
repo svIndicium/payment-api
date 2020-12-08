b2477beec36b7cedb520470813caac0f1cf9bf83
joostlek <joostlek@outlook.com>
2020-12-08 16:45:02 +0100

    Update application test

--- src/test/java/hu/indicium/dev/payment/PaymentServiceApplicationTests.java
+++ src/test/java/hu/indicium/dev/payment/PaymentServiceApplicationTests.java
@@ -1,13 +1,32 @@
 package hu.indicium.dev.payment;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@DisplayName("Application")
+@Tag("Application")
 class PaymentServiceApplicationTests {
 
 	@Test
+	@DisplayName("Test context loading")
 	void contextLoads() {
+		assertTrue(true);
+	}
+
+	@Test
+	@DisplayName("Test startup")
+	void main() {
+		PaymentServiceApplication.main(new String[]{});
+		assertThat(true).isTrue();
 	}
 
 }
