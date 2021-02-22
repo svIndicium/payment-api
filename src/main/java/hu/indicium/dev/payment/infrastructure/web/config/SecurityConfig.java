6367ef4cb4edee5f0beed312af1a30a269f7f678
joostlek <joostlek@outlook.com>
2021-02-22 13:59:43 +0100

    Remove Auth0 remnants

--- src/main/java/hu/indicium/dev/payment/infrastructure/web/config/SecurityConfig.java
+++ src/main/java/hu/indicium/dev/payment/infrastructure/web/config/SecurityConfig.java
@@ -2,18 +2,11 @@ package hu.indicium.dev.payment.infrastructure.web.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -21,12 +14,6 @@ import java.util.List;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final ObjectMapper objectMapper;
-
-    @Value("${auth0.apiAudience}")
-    private String apiAudience;
-
-    @Value("${auth0.issuer}")
-    private String issuer;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
