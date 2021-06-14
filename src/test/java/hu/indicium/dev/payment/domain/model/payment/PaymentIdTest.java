f44e2d330341235d5ee9cfdc5573336af03c65fc
joostlek <joostlek@outlook.com>
2021-06-14 09:35:14 +0200

    Create test for PaymentId

---
+++ src/test/java/hu/indicium/dev/payment/domain/model/payment/PaymentIdTest.java
@@ -0,0 +1,27 @@
+package hu.indicium.dev.payment.domain.model.payment;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DisplayName("PaymentId")
+class PaymentIdTest {
+
+    @Test
+    public void shouldCreatePaymentId() {
+        UUID uuid = UUID.randomUUID();
+        PaymentId paymentId = PaymentId.fromId(uuid);
+        assertThat(paymentId.getId()).isEqualTo(uuid);
+    }
+
+    @Test
+    public void shouldEqualWhenTwoPaymentIdWithSameUuid() {
+        UUID uuid = UUID.randomUUID();
+        PaymentId paymentId = PaymentId.fromId(uuid);
+        PaymentId paymentId1 = PaymentId.fromId(uuid);
+        assertThat(paymentId).isEqualTo(paymentId1);
+    }
+}
